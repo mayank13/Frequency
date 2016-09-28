30e48218e59cc8ed31a0cbcf9814519db1def7de
Mayank Jain <mayank13@gmail.com>
2016-09-28 16:12:32 +0530

    Dummy Layout of the radio news

---
+++ app/src/main/java/radionews/com/frequency/AudioNewsActivity.java
@@ -0,0 +1,13 @@
+package radionews.com.frequency;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AudioNewsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_audio_news);
+    }
+}
