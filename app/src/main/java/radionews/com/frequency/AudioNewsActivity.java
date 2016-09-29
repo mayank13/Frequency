1a05a7c7b6d31d02e26f3bbdc696094daa8e7db8
Mayank Jain <mayank13@gmail.com>
2016-09-29 14:16:22 +0530

    Primitive news player.

--- app/src/main/java/radionews/com/frequency/AudioNewsActivity.java
+++ app/src/main/java/radionews/com/frequency/AudioNewsActivity.java
@@ -1,13 +1,79 @@
 package radionews.com.frequency;
 
+import android.media.MediaPlayer;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.concurrent.TimeUnit;
 
 public class AudioNewsActivity extends AppCompatActivity {
+    private ImageButton playButton;
+    private MediaPlayer mediaPlayer;
+    private double startTime = 0;
+    private double finalTime = 0;
+    private Handler myHandler = new Handler();;
+    private int backwardTime = 10000;
+    private SeekBar seekbar;
+    private TextView time;
+    public static int oneTimeOnly = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_audio_news);
+
+        playButton = (ImageButton) findViewById(R.id.play_button);
+        time = (TextView)findViewById(R.id.news_timer);
+        mediaPlayer = MediaPlayer.create(this, R.raw.sample);
+        seekbar=(SeekBar)findViewById(R.id.news_seekbar);
+        seekbar.setClickable(false);
+
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Playing sound",Toast.LENGTH_SHORT).show();
+                mediaPlayer.start();
+
+                finalTime = mediaPlayer.getDuration();
+                startTime = mediaPlayer.getCurrentPosition();
+
+                if (oneTimeOnly == 0) {
+                    seekbar.setMax((int) finalTime);
+                    oneTimeOnly = 1;
+                }
+                time.setText(String.format("%d min, %d sec",
+                        TimeUnit.MILLISECONDS.toMinutes((long) finalTime),
+                        TimeUnit.MILLISECONDS.toSeconds((long) finalTime) -
+                                TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes((long) finalTime)))
+                );
+
+                seekbar.setProgress((int)startTime);
+                myHandler.postDelayed(UpdateSongTime,100);
+
+            }
+        });
+
     }
+
+    private Runnable UpdateSongTime = new Runnable() {
+        public void run() {
+            startTime = mediaPlayer.getCurrentPosition();
+            time.setText(String.format("%d min, %d sec",
+
+                    TimeUnit.MILLISECONDS.toMinutes((long) startTime),
+                    TimeUnit.MILLISECONDS.toSeconds((long) startTime) -
+                            TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.
+                                    toMinutes((long) startTime)))
+            );
+            seekbar.setProgress((int)startTime);
+            myHandler.postDelayed(this, 100);
+        }
+    };
 }
